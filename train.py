c2346f5ee1469c4bb9bf6ec36906f4284cd4e4b8
Walter Raftus <wrraftus@uwaterloo.ca>
2020-03-21 03:12:33 -0400

    initial conv network implentation, big time losses tho :(

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import os
 import chess.pgn
 import numpy as np
 from state import State
+import argparse
+import tensorflow as tf
 
 def create_dataset(num_samples=None):
     result_values = {'1/2-1/2': 0, '0-1': -1, '1-0': 1}
@@ -42,5 +44,58 @@ def create_dataset(num_samples=None):
     return np.array(X), np.array(y)
 
 if __name__ == "__main__":
-    X, y = create_dataset(10000)
-    np.savez("data/dataset.npz", X, y)
+    # check if we want to recreate dataset or not
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--recreate_dataset', dest='recreate_dataset', action='store_true',
+                        help="flag to recreate the data set from pgns")
+    parser.set_defaults(recreate_dataset = False)
+    args = parser.parse_args()
+
+    if args.recreate_dataset:
+        # recreate and save dataset
+        X, y = create_dataset(50000)
+        np.savez("data/dataset.npz", X, y)
+    else:
+        # load in dataset
+        dataset = np.load("data/dataset.npz")
+        X = dataset['arr_0']
+        y = dataset['arr_1']
+    print("Loaded in X ", X.shape, " and y ", y.shape)
+
+    # create tensorflow dataset
+    dataset = tf.data.Dataset.from_tensor_slices((X, y))
+    dataset = dataset.shuffle(100).batch(64)
+
+    for item in dataset:
+        print(item)
+        break
+    
+    # create CNN model
+    model = tf.keras.models.Sequential()
+    
+    model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(8,8,5)))
+    model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+    
+    model.add(tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(128, (2, 2), activation='relu', padding='same',  strides=2))
+
+    model.add(tf.keras.layers.Conv2D(128, (1, 1), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(128, (1, 1), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(128, (1, 1), activation='relu'))
+
+    model.add(tf.keras.layers.Dense(1, activation='tanh'))
+
+    model.summary()
+   
+    # compile and train model
+    model.compile(optimizer='adam',
+            loss=tf.keras.losses.MeanSquaredError(),
+            metrics=['accuracy'])
+    history = model.fit(X, y, epochs=10)
+    print(history)
