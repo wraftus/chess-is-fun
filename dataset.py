90a4e3ceeeaf687ab6bdc7c3b8d0200f68ef82bd
Walter Raftus <wrraftus@uwaterloo.ca>
2020-03-27 01:28:05 -0400

    Add saving the net and refactored dataset stuff

---
+++ dataset.py
@@ -0,0 +1,63 @@
+import os
+import chess.pgn
+import numpy as np
+from state import State
+
+class ChessDataset(object):
+    def __init__(self, create, train, num_samples=None):
+        # load in or create new dataset
+        self.X, self.y = self.create_dataset(train, num_samples) if create else self.load_dataset(train)
+        
+        # if we created the dataset, save it to disk so we can load it later
+        if create:
+            file_name = 'datasets/train.npz' if train else 'datasets/val.npz'
+            np.savez(file_name, self.X, self.y)
+
+    # create a numpy array of training examples based on pgns
+    def create_dataset(self, train, num_samples):
+        result_values = {'1/2-1/2': 0, '0-1': -1, '1-0': 1}
+        X, y, = [], []
+        games = 0
+
+        # read in each pgn file
+        pgn_dir = 'train_pgns' if train else 'val_pgns' 
+        for fn in os.listdir(pgn_dir):
+            pgn = open(os.path.join(pgn_dir, fn))
+            while True:
+                # load in next game from pgn
+                try:
+                    game = chess.pgn.read_game(pgn)
+                except Exception:
+                    break
+
+                # get value for result from game
+                result = game.headers['Result']
+                if result not in result_values:
+                    continue
+                value = result_values[result]
+                
+                # go through and serialize each move, and assign it a value
+                board = game.board()
+                num_moves = sum(1 for i in game.mainline_moves())
+                if num_moves < 2:
+                    continue
+                for i, move in enumerate(game.mainline_moves()):
+                    board.push(move)
+                    serialized = State(board).serialize()
+                    X.append(serialized)
+                    y.append(value)#* (i/(num_moves-1)))
+                games += 1
+                print("Parsed game %d, dataset now has %d moves" % (games, len(X)))
+                
+                # check if we have enough data
+                if num_samples is not None and len(X) > num_samples:
+                    return np.array(X), np.array(y)
+        
+        return np.array(X), np.array(y)
+
+    # loads a premade data set of numpy arrays
+    def load_dataset(self, train):
+        # load in dataset
+        file_name = 'datasets/train.npz' if train else 'datasets/val.npz'
+        dataset = np.load(file_name)
+        return dataset['arr_0'], dataset['arr_1']
--- train.py
+++ train.py
@@ -1,101 +1,58 @@
 #!/usr/bin/env python3
-import os
-import chess.pgn
 import numpy as np
-from state import State
 import argparse
+from dataset import ChessDataset
 import tensorflow as tf
-
-def create_dataset(num_samples=None):
-    result_values = {'1/2-1/2': 0, '0-1': -1, '1-0': 1}
-    X, y, = [], []
-    games = 0
-
-    # read in each pgn file
-    for fn in os.listdir("pgns"):
-        pgn = open(os.path.join("pgns", fn))
-        while True:
-            # load in next game from pgn
-            try:
-                game = chess.pgn.read_game(pgn)
-            except Exception:
-                break
-
-            # get value for result from game
-            result = game.headers['Result']
-            if result not in result_values:
-                continue
-            value = result_values[result]
-            
-            # go through and serialize each move, and assign it a value
-            board = game.board()
-            for i, move in enumerate(game.mainline_moves()):
-                board.push(move)
-                serialized = State(board).serialize()
-                X.append(serialized)
-                y.append(value)
-            games += 1
-            print("Parsed game %d, dataset now has %d moves" % (games, len(X)))
-            
-            # check if we have enough data
-            if num_samples is not None and len(X) > num_samples:
-                return np.array(X), np.array(y)
-    
-    return np.array(X), np.array(y)
 
 if __name__ == "__main__":
     # check if we want to recreate dataset or not
     parser = argparse.ArgumentParser()
-    parser.add_argument('--recreate_dataset', dest='recreate_dataset', action='store_true',
-                        help="flag to recreate the data set from pgns")
-    parser.set_defaults(recreate_dataset = False)
+    parser.add_argument('--create_train', dest='create_train', action='store_true',
+                        help="flag to recreate the train dataset from pgns")
+    parser.set_defaults(create_train = False)
+    parser.add_argument('--create_val', dest='create_val', action='store_true',
+                        help="flag to recreate the validation dataset from pgns")
+    parser.set_defaults(create_val = False)
     args = parser.parse_args()
 
-    if args.recreate_dataset:
-        # recreate and save dataset
-        X, y = create_dataset(50000)
-        np.savez("data/dataset.npz", X, y)
-    else:
-        # load in dataset
-        dataset = np.load("data/dataset.npz")
-        X = dataset['arr_0']
-        y = dataset['arr_1']
-    print("Loaded in X ", X.shape, " and y ", y.shape)
+    train_dataset = ChessDataset(args.create_train, True, 50000)
+    val_dataset = ChessDataset(args.create_val, False, 1000000)
+    
+    print("Training Set: X ", train_dataset.X.shape, " and y ", train_dataset.y.shape)
+    print("Validation Set: X ", val_dataset.X.shape, " and y ", val_dataset.y.shape)
 
     # create tensorflow dataset
-    dataset = tf.data.Dataset.from_tensor_slices((X, y))
-    dataset = dataset.shuffle(100).batch(64)
-
-    for item in dataset:
-        print(item)
-        break
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_dataset.X, train_dataset.y))
+    train_dataset = train_dataset.shuffle(100).batch(256)
     
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_dataset.X, val_dataset.y))
+    val_dataset = val_dataset.batch(256)
+
     # create CNN model
     model = tf.keras.models.Sequential()
     
     model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(8,8,5)))
-    model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
-
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(128, (2, 2), activation='relu'))
     
-    model.add(tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same'))
-    model.add(tf.keras.layers.Conv2D(128, (2, 2), activation='relu', padding='same',  strides=2))
-
-    model.add(tf.keras.layers.Conv2D(128, (1, 1), activation='relu'))
-    model.add(tf.keras.layers.Conv2D(128, (1, 1), activation='relu'))
-    model.add(tf.keras.layers.Conv2D(128, (1, 1), activation='relu'))
-
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
     model.add(tf.keras.layers.Dense(1, activation='tanh'))
 
     model.summary()
    
-    # compile and train model
+
+    # compile, train and evaluate model
     model.compile(optimizer='adam',
             loss=tf.keras.losses.MeanSquaredError(),
             metrics=['accuracy'])
-    history = model.fit(X, y, epochs=10)
-    print(history)
+    model.fit(train_dataset, epochs=15)
+    
+    print("Evaluating Model ...")
+    model.evaluate(val_dataset)
+
+    # save model
+    print("Saving model ...")
+    model.save('state_model')
